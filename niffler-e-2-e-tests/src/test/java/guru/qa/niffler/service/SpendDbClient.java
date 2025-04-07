7d57e79c151225473ce61e6e83fafe95c1ef46ef
forilyam <For_ilya_m@mail.ru>
2025-04-07 18:28:38 +0300

    niffler-ng-8: lesson 4.1 homework

---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/service/SpendDbClient.java
@@ -0,0 +1,39 @@
+package guru.qa.niffler.service;
+
+import guru.qa.niffler.data.dao.CategoryDao;
+import guru.qa.niffler.data.dao.SpendDao;
+import guru.qa.niffler.data.dao.impl.CategoryDaoJdbc;
+import guru.qa.niffler.data.dao.impl.SpendDaoJdbc;
+import guru.qa.niffler.data.entity.spend.CategoryEntity;
+import guru.qa.niffler.data.entity.spend.SpendEntity;
+import guru.qa.niffler.model.CategoryJson;
+import guru.qa.niffler.model.SpendJson;
+
+public class SpendDbClient {
+
+  private final SpendDao spendDao = new SpendDaoJdbc();
+  private final CategoryDao categoryDao = new CategoryDaoJdbc();
+
+  public SpendJson createSpend(SpendJson spend) {
+    SpendEntity spendEntity = SpendEntity.fromJson(spend);
+    if (spendEntity.getCategory().getId() == null) {
+      CategoryEntity categoryEntity = getOrCreateCategory(CategoryJson.fromEntity(spendEntity.getCategory()));
+      spendEntity.setCategory(categoryEntity);
+    }
+    return SpendJson.fromEntity(
+        spendDao.create(spendEntity)
+    );
+  }
+
+  public CategoryJson createCategory(CategoryJson category) {
+    CategoryEntity categoryEntity = CategoryEntity.fromJson(category);
+    return CategoryJson.fromEntity(
+        categoryDao.create(categoryEntity)
+    );
+  }
+
+  private CategoryEntity getOrCreateCategory(CategoryJson category) {
+    return categoryDao.findCategoryByUsernameAndCategoryName(category.username(), category.name())
+        .orElseGet(() -> CategoryEntity.fromJson(this.createCategory(category)));
+  }
+}
