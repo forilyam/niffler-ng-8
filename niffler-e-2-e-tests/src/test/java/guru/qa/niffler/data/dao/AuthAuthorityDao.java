36c46456ef8e3466f75a1f8e78d02eb43e0d2509
forilyam <For_ilya_m@mail.ru>
2025-04-10 19:59:09 +0300

    niffler-ng-8: lesson 4.2 homework

---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/dao/AuthAuthorityDao.java
@@ -0,0 +1,7 @@
+package guru.qa.niffler.data.dao;
+
+import guru.qa.niffler.data.entity.auth.AuthorityEntity;
+
+public interface AuthAuthorityDao {
+  AuthorityEntity create(AuthorityEntity authority);
+}
